cabc3e15a9ab919486086179b698024ad2964121
Connor812 <connorsavoy812@gmail.com>
2023-03-27 15:08:27 -0400

    Added start code

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,21 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
+const resolvers = {
+  Query: {
+
+      },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    
+  }
+};
+
+module.exports = resolvers;
