733c86d5da6aa4ade104b6a5e3c0414d1cee1d36
Connor812 <connorsavoy812@gmail.com>
2023-03-28 18:04:44 -0400

    added some new test seeding data

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,29 +1,113 @@
 const db = require('./connection');
-const { User } = require('../models');
+const { 
+  User,
+  FavAnime,
+  SavedAnime,
+  FavManga,
+  SavedManga,
+  Genres
+} = require('../models');
 
 db.once('open', async () => {
 
   console.log('products seeded');
 
   await User.deleteMany();
+  await FavAnime.deleteMany();
+  await SavedAnime.deleteMany();
+  await FavManga.deleteMany();
+  await SavedManga.deleteMany();
+  await Genres.deleteMany();
 
-  await User.create({
-    firstName: 'Pamela',
-    lastName: 'Washington',
-    email: 'pamela@testmail.com',
-    password: 'password12345',
-    orders: [
-      {
-        products: [products[0]._id, products[0]._id, products[1]._id]
-      }
-    ]
-  });
+// Seed Genres
 
-  await User.create({
-    firstName: 'Elijah',
-    lastName: 'Holt',
-    email: 'eholt@testmail.com',
-    password: 'password12345'
+const genres = await Genres.create({
+  genreId: 1,
+  name: "Adventure"
+});
+
+console.log('Genres have been seeded');
+
+  // Seed Favourite Anime
+
+const favAnime = await FavAnime.create({
+  animeId: 2,
+  title: 'Berserk',
+  image: 'htts://berserk.png',
+  episodes: 100,
+  description: 'Awesome Anime',
+  genres: [genres._id],
+  trailer: 'https://berskertrailer.com',
+  duration: '24 min long',
+  rating: 'best of all time',
+  rank: 10
+});
+
+console.log('FavAnime have been seeded');
+
+// Seed Saved Anime
+
+const savedAnime = await SavedAnime.create({
+  animeId: 2,
+  title: 'Berserk',
+  image: 'htts://berserk.png',
+  episodes: 100,
+  description: 'Awesome Anime, saved anime',
+  genres: [genres._id],
+  trailer: 'https://berskertrailer.com',
+  duration: '24 min long',
+  rating: 'best of all time',
+  rank: 10
+});
+
+console.log('SavedAnime has been seeded');
+
+// Seed Fav Manga
+
+const favManga = await FavManga.create({
+  animeId: 2,
+  title: 'Berserk',
+  image: 'htts://berserk.png',
+  episodes: 100,
+  description: 'Awesome Manga, favManga',
+  genres: [genres._id],
+  trailer: 'https://berskertrailer.com',
+  duration: '24 min long',
+  rating: 'best of all time',
+  rank: 10
+});
+
+console.log('FavManga has been seeded');
+
+// Seed Saved Manga
+
+const savedManga = await SavedManga.create({
+  animeId: 2,
+    title: 'Berserk',
+    image: 'htts://berserk.png',
+    episodes: 100,
+    description: 'Awesome Manga, saved Manga',
+    genres: [genres._id],
+    trailer: 'https://berskertrailer.com',
+    duration: '24 min long',
+    rating: 'best of all time',
+    rank: 10
+});
+
+console.log('SavedManga has been seeded');
+
+// Seed User Data
+
+  const user = await User.create({
+    username: 'Connor',
+    firstName: 'Connor',
+    lastName: 'Savoy',
+    email: 'connor@gmail.com',
+    password: 'password123',
+    favAnime: [favAnime._id],
+    savedAnime: [savedAnime._id],
+    favManga: [savedManga._id],
+    savedManga: [savedManga._id]
   });
 
   console.log('users seeded');
