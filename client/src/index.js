56916251ae88205c9907ce748b5e60c55bd9148e
maxSt <stovpovymax@gmail.com>
2023-03-28 13:07:09 -0400

    added bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './assets/css/bootstrap.min.css';
+import './assets/css/style.css';
+ 
 
 ReactDOM.render(
   <React.StrictMode>
