c3f22115146e388d64f8c45c8adaebb7d77f9bb5
Connor812 <connorsavoy812@gmail.com>
2023-03-28 18:05:28 -0400

    changed the user model to better incorperagte the other models that are attached to it

--- server/models/User.js
+++ server/models/User.js
@@ -3,146 +3,13 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const bcrypt = require('bcrypt');
 
-// Genre Schema
-
-const genresSchema = new Schema({
-  genreId: {
-    type: Number,
-    required: true
-  },
-  name: {
-    type: String,
-    required: true
-  }
-});
-
-// Saved Anime Schema
-
-const savedAnimeSchema = new Schema({
-  animeId: {
-    type: Number
-  },
-  title: {
-    type: String,
-    required: true
-  },
-  image: {
-    type: String,
-  },
-  episodes: {
-    type: Number,
-  },
-  description: {
-    type: String
-  },
-  genres: [genresSchema],
-  trailer: {
-    type: String
-  },
-  duration: {
-    type: String
-  },
-  rating: {
-    type: String
-  },
-  rank: {
-    type: Number
-  }
-});
-
-//  Favourite Anime Schema
-
-const favAnimeSchema = new Schema({
-  animeId: {
-    type: Number
-  },
-  title: {
-    type: String,
-    required: true
-  },
-  image: {
-    type: String,
-  },
-  episodes: {
-    type: Number,
-  },
-  description: {
-    type: String
-  },
-  genres: [genresSchema],
-  trailer: {
-    type: String
-  },
-  duration: {
-    type: String
-  },
-  rating: {
-    type: String
-  },
-  rank: {
-    type: Number
-  }
-});
-
-// Saved Manga Schema
-
-const savedMangaSchema = new Schema({
-  mangaId: {
-    type: Number
-  },
-  title: {
-    type: String,
-    required: true
-  },
-  image: {
-    type: String,
-  },
-  chapters: {
-    type: Number,
-  },
-  description: {
-    type: String
-  },
-  genres: [genresSchema],
-  rating: {
-    type: String
-  },
-  rank: {
-    type: Number
-  }
-});
-
-// Favourite Manga Schema
-
-const favMangaSchema = new Schema({
-  mangaId: {
-    type: Number
-  },
-  title: {
-    type: String,
-    required: true
-  },
-  image: {
-    type: String,
-  },
-  chapters: {
-    type: Number,
-  },
-  description: {
-    type: String
-  },
-  genres: [genresSchema],
-  rating: {
-    type: String
-  },
-  rank: {
-    type: Number
-  }
-});
-
-// User Schema
-
 const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  
   firstName: {
     type: String,
     required: true,
@@ -156,17 +23,38 @@ const userSchema = new Schema({
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+    match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/
   },
   password: {
     type: String,
     required: true,
     minlength: 5
   },
-  savedAnime: [savedAnimeSchema],
-  favAnime: [favAnimeSchema],
-  savedManga: [savedMangaSchema],
-  favManga: [favMangaSchema]
+  favAnime: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'FavAnime'
+    }
+  ],
+  savedAnime: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'SavedAnime'
+    }
+  ],
+  favManga: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'FavManga'
+    }
+  ],
+  savedManga: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'SavedManga'
+    }
+  ],
 });
 
 // set up pre-save middleware to create password
