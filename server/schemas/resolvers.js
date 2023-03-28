926b9243b409e5f2b9f9c40abaf981744c537325
Connor812 <connorsavoy812@gmail.com>
2023-03-28 18:07:35 -0400

    started working on the resolvers for the server side to link to apollo

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Product, Category, Order } = require('../models');
+const { User } = require('../models');
+const { update } = require('../models/User');
 const { signToken } = require('../utils/auth');
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
@@ -20,42 +21,42 @@ const resolvers = {
       throw new AuthenticationError('Not logged in');
     },
    
-    checkout: async (parent, args, context) => {
-      const url = new URL(context.headers.referer).origin;
-      const order = new Order({ products: args.products });
-      const line_items = [];
+    // checkout: async (parent, args, context) => {
+    //   const url = new URL(context.headers.referer).origin;
+    //   const order = new Order({ products: args.products });
+    //   const line_items = [];
 
-      const { products } = await order.populate('products');
+    //   const { products } = await order.populate('products');
 
-      for (let i = 0; i < products.length; i++) {
-        const product = await stripe.products.create({
-          name: products[i].name,
-          description: products[i].description,
-          images: [`${url}/images/${products[i].image}`]
-        });
+    //   for (let i = 0; i < products.length; i++) {
+    //     const product = await stripe.products.create({
+    //       name: products[i].name,
+    //       description: products[i].description,
+    //       images: [`${url}/images/${products[i].image}`]
+    //     });
 
-        const price = await stripe.prices.create({
-          product: product.id,
-          unit_amount: products[i].price * 100,
-          currency: 'usd',
-        });
+    //     const price = await stripe.prices.create({
+    //       product: product.id,
+    //       unit_amount: products[i].price * 100,
+    //       currency: 'usd',
+    //     });
 
-        line_items.push({
-          price: price.id,
-          quantity: 1
-        });
-      }
+    //     line_items.push({
+    //       price: price.id,
+    //       quantity: 1
+    //     });
+    //   }
 
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items,
-        mode: 'payment',
-        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${url}/`
-      });
+    //   const session = await stripe.checkout.sessions.create({
+    //     payment_method_types: ['card'],
+    //     line_items,
+    //     mode: 'payment',
+    //     success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+    //     cancel_url: `${url}/`
+    //   });
 
-      return { session: session.id };
-    }
+    //   return { session: session.id };
+    // }
   },
 
   Mutation: {
@@ -88,7 +89,24 @@ const resolvers = {
       const token = signToken(user);
 
       return { token, user };
-    }
+    },
+    addFavAnime: async (parent , { username, favAnime }) => {
+      const user = await User.findOneAndUpdate(
+        { username: username },
+        { $addToSet: { favAnime: favAnime } },
+        { new: true }
+        )
+        .then((updateUser) => {
+          if (!updateUser) {
+            return new Error('No user found.')
+          }
+        
+          return updatedUser;
+
+        }).catch((err) => {
+          return err;
+        })
+    },
   }
 };
 
