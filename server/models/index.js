2db6ce5a74db9d880734503de02abcfaf4de8069
Connor812 <connorsavoy812@gmail.com>
2023-03-28 18:06:08 -0400

    added the new models to the index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,16 @@
 const User = require('./User');
+const SavedAnime = require('./SavedAnime');
+const FavAnime = require('./FavAnime');
+const SavedManga = require('./SavedManga');
+const FavManga = require('./FavManga');
+const Genres = require('./Genres');
 
-module.exports = { User };
+module.exports = { 
+    User, 
+    SavedAnime, 
+    FavAnime, 
+    SavedAnime, 
+    SavedManga, 
+    FavManga, 
+    Genres 
+};
