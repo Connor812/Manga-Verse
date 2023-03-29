cb3b2df3b8f53b58877272c1c6eab99c2fc6603d
Connor812 <connorsavoy812@gmail.com>
2023-03-29 01:28:55 -0400

    needed to export new models and remove old ones

--- server/models/index.js
+++ server/models/index.js
@@ -1,16 +1,11 @@
 const User = require('./User');
-const SavedAnime = require('./SavedAnime');
-const FavAnime = require('./FavAnime');
-const SavedManga = require('./SavedManga');
-const FavManga = require('./FavManga');
+const Anime = require('./Anime');
+const Manga = require('./Manga');
 const Genres = require('./Genres');
 
 module.exports = { 
     User, 
-    SavedAnime, 
-    FavAnime, 
-    SavedAnime, 
-    SavedManga, 
-    FavManga, 
+    Anime,
+    Manga,
     Genres 
 };
