0a477f391ad84dde1f820cb9e65661d248d7bb9a
Connor812 <connorsavoy812@gmail.com>
2023-03-29 13:40:01 -0400

    fixed the footer bug by changing the css

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,7 @@ import {Navbar, Card, Container} from 'react-bootstrap';
 const Footer = () => {
   return (
     <Navbar className='Footer' bg='dark' variant='dark' expand='xxl'>
-        <Card.Body fluid>
+        <Card.Body>
             <Card.Title className='FooterTitle'>&copy; 2023 MangaVerse. All Rights Reserved.</Card.Title>
         <Card.Footer>
     <h3>Footer</h3>
