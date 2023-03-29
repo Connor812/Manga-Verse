2757b3b5695a6268fc6a478f196a60964ff0abfa
maxSt <stovpovymax@gmail.com>
2023-03-29 13:43:57 -0400

    move forms

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,20 +11,13 @@ const Home = () => {
   return (
     <div className="home-wrapper">
 
-      <div className="search-wrapper">
+      <div className="anime-manga-wrapper">
 
-        <div className="search-form">
-          <form>
-            <input></input>
-            <button>Search!</button>
-          </form>
-        </div>
-        
         <div className="home-anime-maga">
           <button className="home-btn btn btn-info" onClick={() => setCurrentPage(true)}>Anime</button>
           <button className="home-btn btn btn-info" onClick={() => setCurrentPage(false)}>Manga</button>
         </div>
-
+        
       </div>
 
       <div className="home-content">
