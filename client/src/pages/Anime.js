50bac9eb0cf3d8afcde0801286f36cea66d8164b
Connor812 <connorsavoy812@gmail.com>
2023-03-29 17:05:51 -0400

    added the api link, as well as the onChange handler and the on submit handler that fetches the anime information

--- client/src/pages/Anime.js
+++ client/src/pages/Anime.js
@@ -1,20 +1,54 @@
 import React, { useState } from 'react';
+import Anime_Card from '../components/Anime_Card';
+import '../assets/css/anime.css';
+const url = 'https://api.jikan.moe/v4/anime';
 
 
-const Anime = () => {
+function Anime() {
+
+	const [search, setSearchState] = useState('');
+	const [result, setResultState] = useState([]);
+
+	const handleChange = (event) => {
+		const search = event.target.value;
+		console.log(search);
+		setSearchState(search);
+	}
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		if (search === '') return;
+		try {
+			const response = await fetch(`${url}?q=${search}`);
+			const data = await response.json();
+			console.log(data);
+			setResultState(data);
+		} catch (err) {
+
+		}
+	}
+
 	return (
-		<>
-    <h1>Anime</h1>
+		<div className='home-content'>
+			<h1>Anime</h1>
 
-		<div className="search-wrapper">
-        <div className="search-form">
-          <form>
-						<input type="text" placeholder="Search Anime"></input>
-            <button>Search!</button>
-          </form>
-        </div>
+			<div className="search-wrapper">
+				<div className="search-form">
+					<form>
+						<input type="text" placeholder="Search Anime" onChange={handleChange} />
+						<button onClick={handleSubmit}>Search!</button>
+					</form>
+				</div>
 			</div>
-		</>
+			<div className='row'>
+				{!result.data
+					? console.log('error')
+					: result.data.map((anime) => {
+						return <Anime_Card animes={anime} />
+					})}
+			</div>
+
+		</div>
 	)
 }
 
