6c5151af5b2798d0bc1780a29e0cc3698f600c2b
Connor812 <connorsavoy812@gmail.com>
2023-03-30 18:17:22 -0400

    added the addAnime function and got it working so it saves information to the data base

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -33,3 +33,91 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const ADD_GENRE = gql`
+mutation addGenre(
+  $genreId: Int
+  $name: String!
+) {
+  addGenre(
+    genreId: $genreId
+    name: $name
+  )
+}
+`;
+
+export const ADD_ANIME = gql`
+    mutation addAnime(
+    $animeId: Int!, 
+    $image: String, 
+    $title: String!, 
+    $title_japanese: String, 
+    $episodes: Int, 
+    $trailer: String, 
+    $description: String, 
+    $duration: String, 
+    $rank: Int, 
+    $genres: [String], 
+    $studios: String, 
+    $rating: String
+    ) {
+  addAnime(
+  animeId: $animeId, 
+  image: $image, 
+  title: $title, 
+  title_japanese: $title_japanese, 
+  episodes: $episodes, 
+  trailer: $trailer, 
+  description: $description, 
+  duration: $duration, 
+  rank: $rank, 
+  genres: $genres, 
+  studios: $studios, 
+  rating: $rating
+  ) {
+    animeId
+    image
+    title
+    title_japanese
+    episodes
+    trailer
+    description
+    duration
+    rank
+    studios
+    rating
+    genres {
+      _id
+    }
+  }
+  }
+`;
+
+export const ADD_MANGA = gql`
+mutation addManga(
+  $mangaId: Int!
+  $title: String!
+  $title_japanese: String
+  $image: String
+  $chapters: Int
+  $description: String
+  $rank: Int
+  $author: String
+  $genres: [ID]
+) {
+  addManga(
+    mangaId: $mangaId
+    title: $title
+    title_japanese: $title_japanese
+    image: $image 
+    chapters: $chapters
+    description: $description
+    rank: $rank
+    author: $author
+    genres: $genres
+  )
+}
+
+
+
+`;
