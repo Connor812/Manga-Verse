81abe4b7be4910ac37ef70b4a6610f88152f9bda
Connor812 <connorsavoy812@gmail.com>
2023-03-30 18:26:22 -0400

    added the singleAnime code

--- client/src/pages/singleAnime.js
+++ client/src/pages/singleAnime.js
@@ -33,6 +33,44 @@ const SingleAnime = ()  => {
       }
     }
     animeTrailer();
+
+    return (
+        <div className='single-anime-wrapper'>
+  
+            <div className='single-anime-content'>
+  
+              <h2 className='single-anime-title'>{anime.title}</h2>
+              <h2 className='single-anime-title_japanese'>{anime.title_japanese}</h2>
+  
+              <img className='single-anime-img' src={anime.image} alt={anime.title} />
+  
+              <div className='single-anime-info'>
+                <p className='single-anime-genres'>Genres: {animeGenres()}</p>
+  
+                <p className='single-anime-rating'>
+                  Rating: {anime.rating ? anime.rating : '-'}
+                </p>
+  
+                <p className='single-anime-genres'>
+                  Episodes: {anime.episodes ? anime.episodes : '-'}, 
+                  duration: {anime.duration ? anime.duration : '-'}
+                </p>
+              </div>
+  
+              <p className='single-amine-description'>
+                {anime.description ? anime.description : "Sorry, we don't have description"}
+              </p>
+  
+              <a href={anime.trailer} style={displayTrailer}>
+                <button className='single-amine-button'>
+                  Watch the trailer
+                </button>
+              </a>
+  
+            </div>
+   
+        </div>
+      )
 }
 
 export default SingleAnime;
