443a0bd384d82c50e48d3cb0872a82dc56a2c6d2
Big Bob <devon.eadie@yahoo.com>
2023-03-31 11:41:32 -0700

    linked favorite and save features to profiles

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -25,7 +25,7 @@ const userData = Auth.loggedIn() ? Auth.getProfile() : null
             {Auth.loggedIn() ? (
               <>
                 <Nav.Link as={Link} to='/profile'>
-                  {userData.username}'s Profile
+                  {userData.firstName}'s Profile
                 </Nav.Link>
                 <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
               </>
--- client/src/pages/AnimeProfile.js
+++ client/src/pages/AnimeProfile.js
@@ -1,18 +1,32 @@
-import React, { useState } from 'react';
-import Auth from '../utils/auth';
-import '../assets/css/profile.css';
+import React, { useState } from "react";
+import Auth from "../utils/auth";
+import "../assets/css/profile.css";
+import { GET_ME } from "../utils/queries";
+import { useQuery } from "@apollo/client";
 
 const AnimeProfile = () => {
-	return (
-		<div>
-		<div className='SavedAnime'>
-			<h3>Your Saved Anime</h3>
-		</div>
-		<div className='FavoritedAnime'>
-			<h3>Your Favorited Anime</h3>
-		</div>
-		</div>
-	)
-}
+  const {loading, data} = useQuery(GET_ME);
+  const userData = Auth.loggedIn ? Auth.getProfile() : null;
+  console.log(data)
+  return (
+	<>
+	  {loading ? (
+		<h1>loading</h1>
+		) : (
+	<div>
+      <div className="SavedAnime">
+        <h3>Your Saved Anime</h3>
+		<p></p>
+      </div>
+      <div className="FavoritedAnime">
+        <h3>Your Favorited Anime</h3>
+		<p>{data.me.favAnime[0].title}</p>
+		<p></p>
+      </div>
+	</div>
+	  )
+	}
+	</>
+  )};
 
 export default AnimeProfile;
