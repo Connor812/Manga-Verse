78f26b2b100051905ebb8b4a3a5680b797fa6bb6
Connor812 <connorsavoy812@gmail.com>
2023-04-01 18:39:13 -0400

    changed ip address

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/manga-verse', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://65.95.195.133:27017/manga-verse', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
