a9eaa444173be2c79d71f1680ba3223a010e362a
Connor812 <connorsavoy812@gmail.com>
2023-04-01 18:15:43 -0400

    added the ip back to the connections js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/manga-verse', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/manga-verse', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 module.exports = mongoose.connection;
- 
