c9bb0784ba297110694bfa589d8068d8239f205a
Connor812 <connorsavoy812@gmail.com>
2023-04-01 19:26:09 -0400

    trying to fix the render issue when you save an anime then you try and view it in your profile, it wasnt showing up

--- client/src/pages/AnimeProfile.js
+++ client/src/pages/AnimeProfile.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import Auth from "../utils/auth";
 import { Link } from "react-router-dom";
 import "../assets/css/profile.css";
@@ -7,9 +7,13 @@ import { useQuery } from "@apollo/client";
 import Anime_Profile_Card from "../components/Anime_Profile_Card";
 
 const AnimeProfile = () => {
-  const { loading, data } = useQuery(GET_ME);
+  const { loading, data, refetch } = useQuery(GET_ME);
   const userData = Auth.loggedIn ? Auth.getProfile() : null;
-  console.log(data);
+  
+  useEffect(() => {
+    refetch();
+  }, []);
+
   return (
     <>
       {loading ? (
@@ -19,13 +23,13 @@ const AnimeProfile = () => {
           <div className="FavoritedAnime">
             <div className="displayed-card-wrapper">
             <h3>Your Favorited Anime</h3>
-            {data.me.favAnime.map((anime, index) => { return <Anime_Profile_Card key={index} anime={anime} isFavourite={true} /> })}
+            {loading ? 'Loading' : data.me.favAnime.map((anime, index) => { return <Anime_Profile_Card key={index} anime={anime} isFavourite={true} /> })}
             </div>
           </div>
           <div className="SavedAnime">
             <h3>Your Watchlist Anime</h3>
               <div className="displayed-card-wrapper">
-            {data.me.savedAnime.map((anime, index) => { return <Anime_Profile_Card key={index} anime={anime} isFavourite={false} /> })}
+            {loading ? 'Loading' : data.me.savedAnime.map((anime, index) => { return <Anime_Profile_Card key={index} anime={anime} isFavourite={false} /> })}
             </div>
           </div>
         </div>
