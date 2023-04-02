290ed7dde43c4eca5e601022d38e9ee3cebbe38c
Connor812 <connorsavoy812@gmail.com>
2023-04-02 14:14:25 -0400

    added so whem the token expires and it logs you out, the theme is removed so its set back to the default

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -27,6 +27,7 @@ class AuthService {
     try {
       const decoded = decode(token);
       if (decoded.exp < Date.now() / 1000) {
+        localStorage.removeItem('theme');
         return true;
       } else return false;
     } catch (err) {
